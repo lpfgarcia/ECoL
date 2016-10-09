ed961b01fae7ba1ac61554263ac8c7385ca040cb
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2016-10-09 14:43:39 +0200

    medida t2

--- measures/dimensionality.r
+++ measures/dimensionality.r
@@ -4,25 +4,20 @@
 # The set of Measues based on dimensionality
 
 
-t1 <- function(data) {
-
-
-
-}
-
-
 t2 <- function(data) {
 	aux = nrow(data)/(ncol(data)-1)
 	return(aux)
 }
 
+
 dimensionality <- function(data) {
 
 		aux = lapply(DIMENSIONALITY, 
 		function(i) {
-			do.call(i, list(dst, data))
+			do.call(i, list(data))
 	})
 
 	aux = unlist(aux)
 	return(aux)
 }
+
