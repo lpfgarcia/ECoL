8ca586d251afab909e26427a5ca85ce4203c19a4
aclorena <aclorena@gmail.com>
2017-06-22 16:47:13 -0300

    Update overlapping.r
    I added comments and doubts (with ???) - ACL

--- measures/overlapping.r
+++ measures/overlapping.r
@@ -3,12 +3,44 @@
 # L. P. F. Garcia A. C. Lorena and M. de Souto 2016
 # The set of Overlapping Measures
 
+# Feature overlapping measures from:
+#@article{ho2002complexity,
+#  title={Complexity measures of supervised classification problems},
+#  author={Ho, Tin Kam and Basu, Mitra},
+#  journal={IEEE transactions on pattern analysis and machine intelligence},
+#  volume={24},
+#  number={3},
+#  pages={289--300},
+#  year={2002},
+#  publisher={IEEE}
+#}
 
+# and
+
+#@article{sotoca2005review,
+#  title={A review of data complexity measures and their applicability to pattern classification problems},
+#  author={Sotoca, JM and Sanchez, JS and Mollineda, RA},
+#  journal={Actas del III Taller Nacional de Mineria de Datos y Aprendizaje. TAMIDA},
+#  pages={77--83},
+#  year={2005}
+#}
+
+# and
+
+#@article{orriols2010documentation,
+#  title={Documentation for the data complexity library in C++},
+#  author={Orriols-Puig, Albert and Macia, Nuria and Ho, Tin Kam},
+#  journal={Universitat Ramon Llull, La Salle},
+#  volume={196},
+#  year={2010}
+#}
+
+# Taking all examples with class = j
 branch <- function(data, j) {
 	data[data$class == j, -ncol(data), drop = FALSE]
 }
 
-
+# Means from the same class (numerator from measure F1 from Mollineda)
 num <- function(data, j) {
 
 	tmp = branch(data, j)
@@ -17,7 +49,7 @@ num <- function(data, j) {
 	return(aux)
 }
 
-
+# Standard deviations (denominator from measure F1 from Mollineda)
 den <- function(data, j) {
 
 	tmp = branch(data, j)
@@ -25,7 +57,7 @@ den <- function(data, j) {
 	return(aux)
 }
 
-
+# Fisher’s discriminant ratio (F1)
 f1 <- function(data) {
 
 	aux = mapply(function(j) {
@@ -34,10 +66,10 @@ f1 <- function(data) {
 
 	aux[aux == Inf] = NA
 	aux = rowSums(aux, na.rm=TRUE)
-	return(max(aux))
+	return(max(aux)) # taking the maximum between all features
 }
 
-
+# Auxiliary function for F2
 regionOver <- function(data) {
 
 	l = levels(data$class)
@@ -54,15 +86,17 @@ regionOver <- function(data) {
 	return(aux)
 }
 
-
+# Volume of overlap region (F2)
 f2 <- function(data) {
 
-	data = ovo(data);
+	data = ovo(data); # multiclass problems have to be decomposed previously
 	aux = unlist(lapply(data, regionOver))
-	return(sum(aux))
+	return(mean(aux)) # sum is not comparable 
+	# ??? take the maximum???
 }
 
-
+# Auxiliary function for F3
+# Examples that are not in overlapping region
 nonOverlap <- function(data) {
 
 	l = levels(data$class)
@@ -84,19 +118,20 @@ nonOverlap <- function(data) {
 	return(aux)
 }
 
-
+# Maximum (individual) feature efficiency (F3)
 f3 <- function(data) {
 
-	data = ovo(data);
+	data = ovo(data); # multiclass problems have to be decomposed previously
 	aux = mapply(function(d) {
 		colSums(nonOverlap(d))/nrow(d)
 	}, d=data)
 
-	aux = mean(colMax(aux))
+	aux = mean(colMax(aux)) # ??? take the average or maximum?
 	return(aux)
 }
 
-
+# Auxiliary function for F4
+# Removes points in overlapping regions
 removing <- function(data) {
 
 	repeat {
@@ -112,20 +147,20 @@ removing <- function(data) {
 	return(data)
 }
 
-
+# Collective Feature Efficiency (F4)
 f4 <- function(data) {
 
-	data = ovo(data);
+	data = ovo(data); # multiclass problems have to be decomposed previously
 	aux = mapply(function(d) {
-		n = removing(d)
+		n = removing(d) # removes points in overlapping regions
 		(nrow(d) - nrow(n))/nrow(d)
 	}, d=data)
 
-	aux = mean(aux)
+	aux = mean(aux) # or max???
 	return(aux)
 }
 
-
+# Applying all measures from this category
 fisher <- function(data) {
 
 	data = binarize(data)
