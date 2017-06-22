2f10b8c149515bbbf68760c5b3d930f682364c09
aclorena <aclorena@gmail.com>
2017-06-22 16:34:34 -0300

    Update noise.r
    Some comments and doubts (with ???) - ACL

--- measures/noise.r
+++ measures/noise.r
@@ -3,17 +3,30 @@
 # L. P. F. Garcia A. C. Lorena and M. de Souto 2016
 # The set of Noise Measures
 
+# These measures are from:
+#@inproceedings{van2007measures,
+#  title={Measures for the characterisation of pattern-recognition data sets},
+#  author={Van Der Walt, Christiaan and Barnard, Etienne},
+#  year={2007},
+#  organization={18th Annual Symposium of the Pattern Recognition Association of South Africa (PRASA)}
+#}
 
+# Input noise (N1 from Van der Walt, ID1)
+# Count for each sample in how many dimensions it overlaps and then normalise the total overlap with the product of the number of samples in the data set and the dimensionality of the data set.
+# First applies PCA to decorrelate the features
 id1 <- function(data) {
 
 	data = binarize(data)
+	# ??? Why not scale. and center = TRUE?
 	aux = data.frame(prcomp(data[,-ncol(data)])$x)
 	aux$class = data$class
 	aux = f2(aux)
+	# ??? This is not counting the number of dimensions in which each sample overlap, but it is computing the volume of overlap instead
+	# Definition: count for each sample in how many dimensions it overlaps and then normalise the total overlap with the product of the number of samples in the data set and the dimensionality of the data set.
 	return(aux)
 }
 
-
+# Mutual information of a dataset
 mutual <- function(data) {
 
 	aux = sapply(data, function(i) {
@@ -24,22 +37,23 @@ mutual <- function(data) {
 	return(aux)
 }
 
-
+# Intrinsic dimensionality of Van der Walt
 dimen <- function(data) {
 	aux = mutual(data)
 	aux = sort(aux, decreasing = TRUE)[-1]
-	aux = which(cumsum(aux)/sum(aux) >= 0.9)
+	aux = which(cumsum(aux)/sum(aux) >= 0.9) # number for which cumulative sum to mutual information is above 0.9
 	return(aux[1])
 }
 
-
+# Feature noise (ID2)
+# Proportion of features that do not contribute to classification
 id2 <- function(data) {
 	aux = (ncol(data) - 1 - dimen(data))/
 	(ncol(data) - 1)
 	return(aux) 
 }
 
-
+# Applying all noise measures
 noise <- function(data) {
 
 	aux = lapply(NOISE, 
