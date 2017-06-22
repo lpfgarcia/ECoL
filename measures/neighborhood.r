42fa4a5fa85f76ae5064e478b581cd1f7bcd2909
aclorena <aclorena@gmail.com>
2017-06-22 16:20:18 -0300

    Update neighborhood.r
    I added some comments and doubts (with ???) - ACL

--- measures/neighborhood.r
+++ measures/neighborhood.r
@@ -3,19 +3,40 @@
 # L. P. F. Garcia A. C. Lorena and M. de Souto 2016
 # The set of Measues based on Non Linearity
 
+# Implements measures from:
+#@article{ho2002complexity,
+#  title={Complexity measures of supervised classification problems},
+#  author={Ho, Tin Kam and Basu, Mitra},
+#  journal={IEEE transactions on pattern analysis and machine intelligence},
+#  volume={24},
+#  number={3},
+#  pages={289--300},
+#  year={2002},
+#  publisher={IEEE}
+#}
 
+# and 
+
+# @inproceedings{van2007measures,
+#  title={Measures for the characterisation of pattern-recognition data sets},
+#  author={Van Der Walt, Christiaan and Barnard, Etienne},
+#  year={2007},
+#  organization={18th Annual Symposium of the Pattern Recognition Association of South Africa (PRASA)}
+#}
+
+# Fraction of points on class boundary (N1)
 n1 <- function(dst, data) {
 
-	g = graph.adjacency(dst, weighted = TRUE)
-	tree = as.matrix(as_adj(mst(as.undirected(g))))
+	g = graph.adjacency(dst, weighted = TRUE) # adjacency graph between all pairs of examples in a data set
+	tree = as.matrix(as_adj(mst(as.undirected(g)))) # taking the minimum spanning tree of the previous graph
 
 	tmp = which(tree != 0, arr.ind = TRUE)	
-	aux = which(data[tmp[,1],]$class != data[tmp[,2],]$class)
+	aux = which(data[tmp[,1],]$class != data[tmp[,2],]$class) # taking the connected examples in the MST that have different classes
 	aux = length(unique(tmp[aux,1]))
 	return(aux/nrow(data))
 }
 
-
+# Distance of an example to its nearest neighbor from the same class
 intra <- function(dst, data, i) {
 
 	tmp = rownames(data[data$class == data[i,]$class,])
@@ -23,7 +44,7 @@ intra <- function(dst, data, i) {
 	return(aux)
 }
 
-
+# Distance of an example to its nearest neighbor from a different class
 inter <- function(dst, data, i) {
 
 	tmp = rownames(data[data$class != data[i,]$class,])
@@ -31,7 +52,7 @@ inter <- function(dst, data, i) {
 	return(aux)
 }
 
-
+# Ratio of average intra/inter class NN distance (N2)
 n2 <- function(dst, data) {
 
 	aux = sapply(rownames(data), 
@@ -45,14 +66,16 @@ n2 <- function(dst, data) {
 	return(aux)
 }
 
-
+# Nearest neighor classifier
+# ??? why did you not use an implementation from the R packages??
 knn <- function(dst, data, k, i) {
-	tmp = names(sort(dst[i,])[1:k+1])
+	tmp = names(sort(dst[i,])[1:k+1]) # you do not have to test setdiff(tmp, i)????
 	aux = data[tmp,]$class
 	names(aux) = tmp
-	return(aux)
+	return(aux) 
 }
 
+# Error rate of 1NN classifier (N3)
 n3 <- function(dst, data) {
 
 	aux = unlist(
@@ -65,10 +88,11 @@ n3 <- function(dst, data) {
 	return(mean(aux))
 }
 
-
+# Nonlinearity of 1NN classifier
+# ??? Will you generate the same data as for L3? Probably not, so wouldn't it be interesting to set the same seed for both??
 n4 <- function(dst, data) {
 
-	aux = rbind(data, generate(data))
+	aux = rbind(data, generate(data)) # generating interpolated data
 	vet = setdiff(rownames(aux), rownames(data))
 	dst = dist(aux[,-ncol(aux)])
 
@@ -82,26 +106,28 @@ n4 <- function(dst, data) {
 	return(mean(aux))
 }
 
-
+# Auxiliary function for computing T1
+# Computes the radios of the hyperspheres that can be formed around each example
 radios <- function(dst, data, i) {
 
-	di = inter(dst, data, i)
-	j = names(di)
-	dj = inter(dst, data, j)
-	k = names(dj)
+	di = inter(dst, data, i) # finding i's nearest example from the other class
+	j = names(di) # index of this example
+	dj = inter(dst, data, j) # finding i's nearest example from the other class
+	k = names(dj) # index of the previous example
 
-	if(i == k) {
-		return(di/2)
+	if(i == k) { # if they are mutually the nearest neighbors from different classes
+		return(di/2) # their radios is half the distance between them
 	} else {
-		tmp = radios(dst, data, j)
-		return(dj - tmp)
+		tmp = radios(dst, data, j) # the radios of j must be found recursively
+		return(dj - tmp) # after the radios of j is found, the radios of i can be updated
 	}
 }
 
-
+# Auxiliary function for computing T1
+# Calls the radios function for all examples in a data frame
 hyperspher <- function(dst, data) {
 
-	r = rep(0, nrow(data))
+	r = rep(0, nrow(data)) # initialization
 	names(r) = rownames(data) 
 
 	for(i in names(r)){
@@ -111,7 +137,8 @@ hyperspher <- function(dst, data) {
 	return(r)
 }
 
-
+# Auxiliary function for computing T1
+# ??? verifying which hyperspheres can be eliminated
 translate <- function(dst, r) {
 
 	aux = do.call("rbind",
@@ -125,7 +152,8 @@ translate <- function(dst, r) {
 	return(aux)
 }
 
-
+# Auxiliary function for computing T1
+# ???
 adherence <- function(adh, data) {
 
 	h = n = c()
@@ -149,7 +177,7 @@ adherence <- function(adh, data) {
 	return(h)
 }
 
-
+# Fraction of points with associated adherence subsets retained (T1)
 t1 <- function(dst, data) {
 
 	r = hyperspher(dst, data)
@@ -158,7 +186,8 @@ t1 <- function(dst, data) {
 	return(aux)
 }
 
-
+# Samples per group (T2 from Van der Walts)
+# average number of samples per group (adherence subset)
 t2 <- function(dst, data) {
 
 	r = hyperspher(dst, data)
@@ -166,21 +195,23 @@ t2 <- function(dst, data) {
 	return(mean(aux))
 }
 
-
+# ???
 ball <- function(r, n) {
 	(1/sqrt(n*pi))*((2*pi*exp(1)/n)^(n/2))*r^n
 }
 
-
+# Inter-class scale variation (part of T4 from Van der Walts) 
+# density of the hyper-spheres retained
+# SD of sphere densities is T4, here it is pho from Equation 3
 t4 <- function(dst, data) {
 
 	r = hyperspher(dst, data)
 	aux = adherence(translate(dst, r), data)
 	tmp = aux/ball(r[names(aux)], ncol(data)-1)
-	return(mean(tmp))
+	return(std(tmp)) # you took the mean, but according to the description, you should take the standard deviation
 }
 
-
+# Applying all neighborhood functions
 neighborhood <- function(data) {
 
 	dst = dist(data[,-ncol(data)])
