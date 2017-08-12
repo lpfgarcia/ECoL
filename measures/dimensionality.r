99518480dfb78975442dc4190df96f7f6b29ae60
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-08-12 17:48:33 +0200

    closing issuues 4

--- measures/dimensionality.r
+++ measures/dimensionality.r
@@ -2,7 +2,7 @@
 # Measures based on dimensionality
 # L. P. F. Garcia A. C. Lorena and M. de Souto 2016
 # The set of Measues based on dimensionality
-
+#
 # These measures are described in:
 #@article{lorena2012analysis,
 #  title={Analysis of complexity indices for classification problems: Cancer gene expression data},
@@ -15,47 +15,30 @@
 #  publisher={Elsevier}
 #}
 
-# Log of the average number of features per example (M1 or T2')
-# This measure is similar to T2
-# Caution: this measure will take negative values for datasets containing more examples than features 
-m1 <- function(data) {
-	log((ncol(data)-1)/nrow(data))
-}
 
-# Auxiliary function for taking the number of components representing 95% of data variability in PCA
 pca <- function(data) {
-# ????
-	data = binarize(data) # why do you need this?
-	# usually I use center = TRUE and scale. = TRUE in PCA
-	aux = prcomp(data[,-ncol(data)])$sdev
-	aux = which(cumsum(aux)/sum(aux) >= 0.95)
-	return(aux[1]) # taking the number of components representing 95% of data variability
+
+    aux = prcomp(data[,-ncol(data)])$sdev
+    aux = which(cumsum(aux)/sum(aux) >= 0.95)
+    return(aux[1])
 }
 
-# Principal component dimensionality/sample ratio (M2)
-# Log of the ratio of the number of components representing 95% of data variability and the number of examples in a dataset
-# Caution: this measure will take negative values for datasets containing more examples than PCA components explaining 95% of data variability
-# ??? Is it better to remove the log?
-m2 <- function(data) {
-	log(pca(data)/nrow(data))
-}
 
-# Principal component dimensionality/dimensionality ratio (M3)
-# Ratio of the number of components representing 95% of data variability and the number of original features in a dataset
 m3 <- function(data) {
-	pca(data)/(ncol(data)-1)
+    pca(data)/(ncol(data)-1)
 }
 
-# Auxiliary function for applying the measures
+
 dimensionality <- function(data) {
 
-	aux = lapply(DIMENSIONALITY, 
-		function(i) {
-			do.call(i, list(data))
-	})
+    data = binarize(normalize(data))
+    aux = lapply(DIMENSIONALITY, 
+        function(i) {
+            do.call(i, list(data))
+    })
 
-	aux = unlist(aux)
-	names(aux) = DIMENSIONALITY
-	return(aux)
+    aux = unlist(aux)
+    names(aux) = DIMENSIONALITY
+    return(aux)
 }
 
--- measures/overlapping.r
+++ measures/overlapping.r
@@ -1,9 +1,8 @@
 # R Code
 # Overlapping Measures
-# L. P. F. Garcia A. C. Lorena and M. de Souto 2016
+# L. P. F. Garcia A. C. Lorena and M. de Souto 2017
 # The set of Overlapping Measures
-
-# Feature overlapping measures from:
+#
 #@article{ho2002complexity,
 #  title={Complexity measures of supervised classification problems},
 #  author={Ho, Tin Kam and Basu, Mitra},
@@ -14,9 +13,7 @@
 #  year={2002},
 #  publisher={IEEE}
 #}
-
-# and
-
+#
 #@article{sotoca2005review,
 #  title={A review of data complexity measures and their applicability to pattern classification problems},
 #  author={Sotoca, JM and Sanchez, JS and Mollineda, RA},
@@ -24,9 +21,7 @@
 #  pages={77--83},
 #  year={2005}
 #}
-
-# and
-
+#
 #@article{orriols2010documentation,
 #  title={Documentation for the data complexity library in C++},
 #  author={Orriols-Puig, Albert and Macia, Nuria and Ho, Tin Kam},
@@ -35,143 +30,140 @@
 #  year={2010}
 #}
 
-# Taking all examples with class = j
+
 branch <- function(data, j) {
-	data[data$class == j, -ncol(data), drop = FALSE]
+    data[data$class == j, -ncol(data), drop=FALSE]
 }
 
-# Means from the same class (numerator from measure F1 from Mollineda)
+
 num <- function(data, j) {
 
-	tmp = branch(data, j)
-	aux = nrow(tmp) * (colMeans(tmp) - 
-		colMeans(data[,-ncol(data)]))^2
-	return(aux)
+    tmp = branch(data, j)
+    aux = nrow(tmp) * (colMeans(tmp) - 
+        colMeans(data[,-ncol(data)]))^2
+    return(aux)
 }
 
-# Standard deviations (denominator from measure F1 from Mollineda)
+
 den <- function(data, j) {
 
-	tmp = branch(data, j)
-	aux = rowSums((t(tmp) - colMeans(tmp))^2)
-	return(aux)
+    tmp = branch(data, j)
+    aux = rowSums((t(tmp) - colMeans(tmp))^2)
+    return(aux)
 }
 
-# Fisher’s discriminant ratio (F1)
+
 f1 <- function(data) {
 
-	aux = mapply(function(j) {
-		num(data, j)/den(data, j)
-	}, j=levels(data$class))
+    aux = mapply(function(j) {
+        num(data, j)/den(data, j)
+    }, j=levels(data$class))
 
-	aux[aux == Inf] = NA
-	aux = rowSums(aux, na.rm=TRUE)
-	return(max(aux)) # taking the maximum between all features
+    aux[aux == Inf] = NA
+    aux = rowSums(aux, na.rm=TRUE)
+    return(max(aux))
 }
 
-# Auxiliary function for F2
+
 regionOver <- function(data) {
 
-	l = levels(data$class)
-	a = branch(data, l[1])
-	b = branch(data, l[2])
+    l = levels(data$class)
+    a = branch(data, l[1])
+    b = branch(data, l[2])
 
-	maxmax = rbind(colMax(a), colMax(b))
-	minmin = rbind(colMin(a), colMin(b))
+    maxmax = rbind(colMax(a), colMax(b))
+    minmin = rbind(colMin(a), colMin(b))
 
-	over = colMax(rbind(colMin(maxmax) - colMax(minmin), 0))
-	rang = colMax(maxmax) - colMin(minmin)
+    over = colMax(rbind(colMin(maxmax) - colMax(minmin), 0))
+    rang = colMax(maxmax) - colMin(minmin)
 
-	aux = prod(over/rang, na.rm = TRUE)
-	return(aux)
+    aux = prod(over/rang, na.rm=TRUE)
+    return(aux)
 }
 
-# Volume of overlap region (F2)
+
 f2 <- function(data) {
 
-	data = ovo(data); # multiclass problems have to be decomposed previously
-	aux = unlist(lapply(data, regionOver))
-	return(mean(aux)) # sum is not comparable 
-	# ??? take the maximum???
+    data = ovo(data)
+    aux = unlist(lapply(data, regionOver))
+    return(mean(aux)) # sum is not comparable 
+    # ??? take the maximum???
 }
 
-# Auxiliary function for F3
-# Examples that are not in overlapping region
+
 nonOverlap <- function(data) {
 
-	l = levels(data$class)
-	a = branch(data, l[1])
-	b = branch(data, l[2])
+    l = levels(data$class)
+    a = branch(data, l[1])
+    b = branch(data, l[2])
 
-	minmax = colMin(rbind(colMax(a), colMax(b)))
-	maxmin = colMax(rbind(colMin(a), colMin(b)))
+    minmax = colMin(rbind(colMax(a), colMax(b)))
+    maxmin = colMax(rbind(colMin(a), colMin(b)))
 
-	aux = do.call("cbind",
-		lapply(1:(ncol(data)-1), 
-			function(i) {
-				data[,i] < maxmin[i] | data[,i] > minmax[i]
-		})
-	)
+    aux = do.call("cbind",
+        lapply(1:(ncol(data)-1), 
+            function(i) {
+                data[,i] < maxmin[i] | data[,i] > minmax[i]
+        })
+    )
 
-	aux = data.frame(aux)
-	rownames(aux) = rownames(data)
-	return(aux)
+    aux = data.frame(aux)
+    rownames(aux) = rownames(data)
+    return(aux)
 }
 
-# Maximum (individual) feature efficiency (F3)
+
 f3 <- function(data) {
 
-	data = ovo(data); # multiclass problems have to be decomposed previously
-	aux = mapply(function(d) {
-		colSums(nonOverlap(d))/nrow(d)
-	}, d=data)
+    data = ovo(data)
+    aux = mapply(function(d) {
+        colSums(nonOverlap(d))/nrow(d)
+    }, d=data)
 
-	aux = mean(colMax(aux)) # ??? take the average or maximum?
-	return(aux)
+    aux = mean(colMax(aux)) # ??? take the average or maximum?
+    return(aux)
 }
 
-# Auxiliary function for F4
-# Removes points in overlapping regions
+
 removing <- function(data) {
 
-	repeat {
-		tmp = nonOverlap(data)
-		col = which.max(colSums(tmp))
-		aux = rownames(tmp[tmp[,col] != TRUE, , drop = FALSE])
-		data = data[aux,- col, drop = FALSE]
-		if(nrow(data) == 0 | ncol(data) == 1 |
-			length(unique(data$class)) == 1)
-			break
-	}
+    repeat {
+        tmp = nonOverlap(data)
+        col = which.max(colSums(tmp))
+        aux = rownames(tmp[tmp[,col] != TRUE, drop=FALSE])
+        data = data[aux,- col, drop=FALSE]
+        if(nrow(data) == 0 | ncol(data) == 1 |
+            length(unique(data$class)) == 1)
+            break
+    }
 
-	return(data)
+    return(data)
 }
 
-# Collective Feature Efficiency (F4)
+
 f4 <- function(data) {
 
-	data = ovo(data); # multiclass problems have to be decomposed previously
-	aux = mapply(function(d) {
-		n = removing(d) # removes points in overlapping regions
-		(nrow(d) - nrow(n))/nrow(d)
-	}, d=data)
+    data = ovo(data)
+    aux = mapply(function(d) {
+        n = removing(d)
+        (nrow(d) - nrow(n))/nrow(d)
+    }, d=data)
 
-	aux = mean(aux) # or max???
-	return(aux)
+    aux = mean(aux)
+    return(aux)
 }
 
-# Applying all measures from this category
+
 fisher <- function(data) {
 
-	data = binarize(data)
+    data = binarize(normalize(data))
+    aux = lapply(OVERLAPPING, 
+        function(i) {
+            do.call(i, list(data))
+    })
 
-	aux = lapply(OVERLAPPING, 
-		function(i) {
-			do.call(i, list(data))
-	})
-
-	aux = unlist(aux)
-	names(aux) = OVERLAPPING
-	return(aux)
+    aux = unlist(aux)
+    names(aux) = OVERLAPPING
+    return(aux)
 }
 
