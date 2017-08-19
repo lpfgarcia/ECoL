712ed0fd6213140bcb3ab04b27fedbe86d478ae7
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-08-19 12:23:10 +0200

    remove comments

--- measures/dimensionality.r
+++ measures/dimensionality.r
@@ -2,18 +2,6 @@
 # Measures based on dimensionality
 # L. P. F. Garcia A. C. Lorena and M. de Souto 2016
 # The set of Measues based on dimensionality
-#
-# These measures are described in:
-#@article{lorena2012analysis,
-#  title={Analysis of complexity indices for classification problems: Cancer gene expression data},
-#  author={Lorena, Ana C and Costa, Ivan G and Spola{\^o}r, Newton and De Souto, Marcilio CP},
-#  journal={Neurocomputing},
-#  volume={75},
-#  number={1},
-#  pages={33--42},
-#  year={2012},
-#  publisher={Elsevier}
-#}
 
 
 pca <- function(data) {
